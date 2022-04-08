1c5c95302e30fd44bbe306e75ae06b8ad9f2947c
Gunjan Solanki <gunjan.solanki@bacancy.com>
2022-04-08 15:34:42 +0530

    Post creation with tdd

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,4 +7,23 @@ describe 'navigate' do
       expect(page.status_code).to eq(200)
     end
   end
+
+  describe 'new' do
+    before do 
+      visit new_post_path
+    end
+
+    it 'can be reached successfully' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it "can be filled with values" do 
+      fill_in "post[date]", with: Date.today
+      fill_in "post[rationale]", with: "Rationale post"
+
+      click_on "Save"
+
+      expect(page).to have_content("Rationale post")
+    end
+  end
 end 
